c15785d5e6682057eecb01aedca5fe9d52190b5e
yasmine <yasmine.ezequiel@gmail.com>
2019-09-05 16:19:32 +0200

    Adds expired date to the account in initialize method and wrote test for it

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,19 @@
+class Account
+    attr_accessor :exp_date
+STANDARD_VALIDITY_YRS = 5
+    
+
+def initialize
+    @exp_date = '09/24'
+    end
+
+    
+end
+
+def set_expire_date
+    Date.today.next_year(Account:: STANDARD_VALIDITY_YRS).strftime( '%m/%Y')
+end
+
 
 
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,11 @@
+require'./lib/account.rb'
+require 'date'
 
+describe Account do
+
+    it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+    end
+
+end
