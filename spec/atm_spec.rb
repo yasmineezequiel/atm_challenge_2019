a29da29975c282a9d356aecfefbf6a44d30033d2
jonas <jonas.masharqa@gmail.com>
2019-09-05 10:13:12 +0200

    Adds method for bills and edits account/amount test to test bills method as well.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -10,8 +10,8 @@ describe Atm do
   end
   
 
-  it 'allow withdraw if the account has enough balance.' do
-    expected_output = { status: true, message: 'success', date: Date.today, amount:45, account_status: :active } 
+  it 'allow withdraw if the account has enough balance and the account is active.' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount:45, account_status: :active, bills: [20, 20, 5]} 
     expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
   
