aa74d058f9ab288d87640749b4609be133769000
yasmine <yasmine.ezequiel@gmail.com>
2019-09-18 19:16:03 +0200

    Adds withdraw method

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -50,7 +50,7 @@ describe Person do
 
         it 'can withdraw funds' do
             command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
-            expect { command.call }.to raise_error 'An ATM is required'
+            expect(command.call).to be_truthy 
         end
 
         it 'withdraw is expected to raise an error if no ATM is passed in' do
